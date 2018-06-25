df49b981013754ac5193cffe67e5aeadaa8c64c7
mcintosh <j.mcintosh@columbia.edu>
2018-06-24 21:01:48 -0400

    added checks for dependencies

--- example/hab_submit_testing.m
+++ example/hab_submit_testing.m
@@ -1,8 +1,17 @@
 clear;
 addpath('..');
-%% 
+%%
+if isempty(which('findjobj.m'))
+    error('You need to add findobj.m to path - see readme.');
+end
+if isempty(which('ssh2.m'))
+    error('You need to add ssh2 library to path - see readme.');
+end
+%%
 username = input('Please enter username usually something like (abc1234):','s');
 account = input('Please enter account (e.g. free):','s');
+submit = false;
+recover = not(submit);
 % username = 'abc1234';
 % account = 'free';
 %%
@@ -19,9 +28,14 @@ d.email = 'email@gmail.com';
 
 matfunc = "hab_testing;";
 %% submit:
-hab_submit(username,account, matfunc, matdir, matname,...
-    'n_par', d.n_par,'walltime', d.walltime,...
-    'email_condition', d.email_condition,'email', d.email);
+if submit
+    hab_submit(username,account, matfunc, matdir, matname,...
+        'n_par', d.n_par,'walltime', d.walltime,...
+        'email_condition', d.email_condition,'email', d.email);
+end
 %%
-% matresult = 'result_directory';
-% hab_recover(username,account,matresult,matname);
+if recover
+    %has to be consistent with hab_testing.m actually does
+    matresult = 'result_directory';
+    hab_recover(username,account,matresult,matname);
+end
