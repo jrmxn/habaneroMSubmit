63e8608105f2a59013ab451a957bc82bd966d261
jrmxn <j.mcintoshr@gmail.com>
2018-06-24 20:47:36 -0400

    added userdir for windows

--- get_password.m
+++ get_password.m
@@ -1,6 +1,8 @@
 function pw = get_password(varargin)
 if ispc
-    error('Need to add this.');
+    userDir = winqueryreg('HKEY_CURRENT_USER',...
+        ['Software\Microsoft\Windows\CurrentVersion\' ...
+         'Explorer\Shell Folders'],'Personal');
 else
     userDir = char(java.lang.System.getProperty('user.home'));
 end
