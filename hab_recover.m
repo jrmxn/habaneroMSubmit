3dc5525cfd49be15527ab0534c4d5acd70225348
jrmxn <j.mcintoshr@gmail.com>
2018-06-24 20:28:38 -0400

    tidied

--- hab_recover.m
+++ hab_recover.m
@@ -1,29 +1,19 @@
 function hab_recover(user,account,matresult,matname,varargin)
-% d.n_par = 24;
-% d.walltime = '12:00:00';
-% d.mem = 4;%gb
 d.pw_ssh = [];
-% d.dosub = true;
+d.scratch_dir = '/rigel';%on remote host
 d.remotehost = 'habanero.rcs.columbia.edu';
 %%
 v = inputParser;
-% addParameter(v,'n_par',d.n_par);
-% addParameter(v,'walltime',d.walltime);
-% addParameter(v,'mem',d.mem);
-addParameter(v,'pw_ssh',d.pw_ssh);
-% addParameter(v,'dosub',d.dosub);
-addParameter(v,'remotehost',d.remotehost);
+addParameter(v,'pw_ssh', d.pw_ssh);
+addParameter(v,'scratch_dir', d.scratch_dir);
+addParameter(v,'remotehost', d.remotehost);
 parse(v,varargin{:});
 v = v.Results;clear d;
 %%
-remotePath = fullfile('/rigel',account,'users');
-remotePath_user = fullfile(remotePath,user);
-remotePath_user_workdir = fullfile(remotePath_user,'Local');
+remotePath = fullfile(v.scratch_dir, account, 'users');
+remotePath_user = fullfile(remotePath, user);
+remotePath_user_workdir = fullfile(remotePath_user, 'Local');
 %%
-% matfunc = 'run_ddm_run_sz_eeg';%function to run within matdir
-% matdir = '/hdd/Cloud/Research/matlab/exp/dm-sz2/analysis/v19';
-% matname = 'sz';%project name
-%
 if isempty(v.pw_ssh)
     v.pw = get_password;
 else
@@ -33,10 +23,6 @@ end
 tar_file = [matname '_' matresult '.tar'];
 localPath = tempname;mkdir(localPath);
 locaPath_subdir = fullfile(localPath,[matname '_' matresult]);mkdir(locaPath_subdir);
-% fprintf('Copying directory to temp directory...\n');
-% copyfile(matdir,locaPath_subdir);
-% fprintf('Archiving it...\n');
-% g = tar(fullfile(localPath,tar_file),locaPath_subdir);
 %%
 if not(exist('ssh2_struct','var')==1)
     pw_clear = v.pw.ssh;
@@ -48,33 +34,35 @@ if not(exist('ssh2_struct','var')==1)
 end
 try
     %%
-    doesex = 'doesex';
-    command = sprintf('[ -d "%s" ] && echo "%s"',fullfile(remotePath_user_workdir,[matname]),doesex);
+    positive_exist_string = 'yes';
+    command = sprintf('[ -d "%s" ] && echo "%s"',...
+        fullfile(remotePath_user_workdir, [matname]), positive_exist_string);
     [ssh2_struct, command_result] = ssh2_command(ssh2_struct, command, false);
-    target_exists = strcmpi(command_result{1},doesex);
+    target_exists = strcmpi(command_result{1}, positive_exist_string);
     if not(target_exists),ssh2_struct = ssh2_close(ssh2_struct);error('Folder does not exist remotely.');end
     %%
     fprintf('Creating archive remotely...\n');
-    command = sprintf('tar -C %s -cvf %s %s',fullfile(remotePath_user_workdir,matname),tar_file,matresult);
+    command = sprintf('tar -C %s -cvf %s %s',...
+        fullfile(remotePath_user_workdir, matname), tar_file, matresult);
     [ssh2_struct, command_result] = ssh2_command(ssh2_struct, command, false);
     %%
     fprintf('Transferring archive.\n');
     ssh2_struct = scp_get(ssh2_struct, tar_file, localPath, '');
-    %%
-    f = untar(fullfile(localPath,tar_file),localPath);
-    if not(exist(fullfile(pwd,[matresult '_x']),'dir')==7),mkdir(fullfile(pwd,[matresult '_x']));end
-    copyfile(fullfile(localPath,matresult),fullfile(pwd,[matresult '_x']));
+    %% Get the matresult directory back and as [matresult '_x']
+    f = untar(fullfile(localPath, tar_file), localPath);
+    if not(exist(fullfile(pwd,[matresult '_x']),'dir')==7)
+        mkdir(fullfile(pwd,[matresult '_x']));
+    end
+    copyfile(fullfile(localPath, matresult), fullfile(pwd, [matresult '_x']));
     %%
     fprintf('Deleting remote archive...\n');
-    command = sprintf('rm %s',tar_file);
+    command = sprintf('rm %s', tar_file);
     [ssh2_struct, command_result] = ssh2_command(ssh2_struct, command, false);
     %%
     fprintf('Closing SSH session...\n');
-    ssh2_struct = ssh2_close(ssh2_struct);
-    
+    ssh2_struct = ssh2_close(ssh2_struct);    
     fprintf('Cleaning up temporary directory locally...\n');
-    % clear pw ssh2_struct;%clear password from memory
-    rmdir(localPath,'s');
+    rmdir(localPath, 's');
     fprintf('Done.\n');
 catch er
     fprintf('Rethrowing error...\n');
--- hab_submit.m
+++ hab_submit.m
@@ -4,10 +4,12 @@ d.walltime = '12:00:00';
 d.mem = 4;%gb
 d.pw_ssh = [];
 d.dosub = true;
+d.scratch_dir = '/rigel';%on remote host
 d.remotehost = 'habanero.rcs.columbia.edu';
 d.force_overwrite = false;
 d.email = '';
 d.email_condition = 'NONE';%END
+d.jobname = '';
 %%
 v = inputParser;
 addParameter(v,'n_par',d.n_par);
@@ -15,31 +17,36 @@ addParameter(v,'walltime',d.walltime);
 addParameter(v,'mem',d.mem);
 addParameter(v,'pw_ssh',d.pw_ssh);
 addParameter(v,'dosub',d.dosub);
+addParameter(v,'scratch_dir',d.scratch_dir);
 addParameter(v,'remotehost',d.remotehost);
 addParameter(v,'force_overwrite',d.force_overwrite);
 addParameter(v,'email',d.email);
 addParameter(v,'email_condition',d.email_condition);
+addParameter(v,'jobname',d.jobname);
 parse(v,varargin{:});
 v = v.Results;clear d;
 %%
-remotePath = fullfile('/rigel',account,'users');
+remotePath = fullfile(v.scratch_dir,account,'users');
 remotePath_user = fullfile(remotePath,user);
 remotePath_user_workdir = fullfile(remotePath_user,'Local');
 %%
-fprintf('Will try to send %s\nto Habanero',matdir);
+fprintf('Will try to send %s\nto HPC',matdir);
 fprintf('and run %s.m\n',matfunc);
 %%
-% matfunc = 'run_ddm_run_sz_eeg';%function to run within matdir
-% matdir = '/hdd/Cloud/Research/matlab/exp/dm-sz2/analysis/v19';
-% matname = 'sz';%project name
-job_id = sprintf('Job%d',randi(100000));
-%
+if isempty(v.jobname)
+    job_id = sprintf('Job%d',randi(100000));
+else
+    job_id = v.jobname;
+end
+%% Get password if not provided and setup SSH connection
 if isempty(v.pw_ssh)
+    %Function that asks user to type password
+    %tries to clear it from Matlab command window and command history
+    %but not guaranteed to work.
     v.pw = get_password;
 else
     v.pw = v.pw_ssh;
 end
-%%
 if not(exist('ssh2_struct','var')==1)
     pw_clear = v.pw.ssh;
     if not(ischar(pw_clear)),error('Password should be a string here - maybe it is nested in a struc?');end
@@ -48,23 +55,23 @@ if not(exist('ssh2_struct','var')==1)
     ssh2_struct = ssh2(ssh2_struct);
     clear pw;
 end
-%%
+%% Setup where we will locally deal with all the file processing etc.
 tar_file = [matname '.tar'];
 localPath = tempname;mkdir(localPath);
 locaPath_subdir = fullfile(localPath,matname);mkdir(locaPath_subdir);
-%%
+%% Look on the HPC to see if the target folder exists
 fprintf('Checking if target folder already exists...')
-doesex = 'doesex';
-command = sprintf('[ -d "%s" ] && echo "%s"',fullfile(remotePath_user_workdir,[matname]),doesex);
+positive_exist_string = 'yes';
+command = sprintf('[ -d "%s" ] && echo "%s"',...
+    fullfile(remotePath_user_workdir,[matname]),positive_exist_string);
 [ssh2_struct, command_result] = ssh2_command(ssh2_struct, command, false);
-target_exists = strcmpi(command_result{1},doesex);
+target_exists = strcmpi(command_result{1},positive_exist_string);
+%% If it doesn't exist tar matdir and upload it
 if not(target_exists)||v.force_overwrite
-    %%
-    
     fprintf('Copying directory to temp directory...\n');
     copyfile(matdir,locaPath_subdir);
     fprintf('Archiving it...\n');
-    g = tar(fullfile(localPath,tar_file),locaPath_subdir);
+    tar(fullfile(localPath,tar_file),locaPath_subdir);
     %%
     fprintf('Transferring archive.\n');
     ssh2_struct = scp_put(ssh2_struct, tar_file, remotePath_user_workdir, localPath, tar_file);
@@ -73,13 +80,13 @@ if not(target_exists)||v.force_overwrite
     command = sprintf('tar -xvf %s -C %s',fullfile(remotePath_user_workdir,tar_file), fullfile(remotePath_user_workdir));
     [ssh2_struct, command_result] = ssh2_command(ssh2_struct, command, false);
     %%
-    fprintf('Deleting tar file...\n');
+    fprintf('Deleting tar file remotely...\n');
     command = sprintf('rm %s',fullfile(remotePath_user_workdir,tar_file));
     [ssh2_struct, command_result] = ssh2_command(ssh2_struct, command, false);
 else
     fprintf('Folder already present. Not overwriting.\n');
 end
-%%
+%% Now write the bash script by replacing strings in hab_basic_submit.shx
 fprintf('Writing bash script...\n');
 Y = fileread(fullfile(fileparts(mfilename('fullpath')),'hab_basic_submit.shx'));
 X = Y;
@@ -112,13 +119,14 @@ sh_file = sprintf('%s.sh',job_id);
 fid = fopen(fullfile(localPath,sh_file),'wt');
 fprintf(fid, '%s',X);
 fclose(fid);
-%% submit script and run
+%% upload script and submit it (sbatch command)
 fprintf('Transmitting and submitting bash script...\n');
 ssh2_struct = scp_put(ssh2_struct, sh_file, remotePath_user_workdir, localPath, sh_file);
 command = sprintf('sbatch %s',fullfile(remotePath_user_workdir,sh_file));
 if v.dosub
     [ssh2_struct, command_result] = ssh2_command(ssh2_struct, command, false);
 else
+    %for testing
     fprintf('Skipping actual sbatch command.\n');
 end
 %%
@@ -126,7 +134,6 @@ fprintf('Closing SSH session...\n');
 ssh2_struct = ssh2_close(ssh2_struct);
 
 fprintf('Cleaning up temporary directory locally...\n');
-% clear pw ssh2_struct;%clear password from memory
 rmdir(localPath,'s');
 fprintf('Done.\n');
 end
